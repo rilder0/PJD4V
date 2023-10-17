51c94d0a63e2b9659847c51b097c023316802c3f
Rilder <79425480+rilder0@users.noreply.github.com>
2023-10-17 17:24:04 -0300

    Update 2

--- PJD4V/Assets/Scripts/BulletController.cs
+++ PJD4V/Assets/Scripts/BulletController.cs
@@ -35,7 +35,7 @@ public class BulletController : MonoBehaviour
     {
         if (other.CompareTag("Enemy"))
         {
-            other.GetComponent<EnemyController>().TakeEnergy(damage);
+            //other.GetComponent<EnemyController>().TakeEnergy(damage);
             Instantiate(bulletExplosion, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
