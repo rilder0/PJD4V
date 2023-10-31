e471f4398135b0fe33bde265903a9f7d69efeda0
Rilder <79425480+rilder0@users.noreply.github.com>
2023-10-31 16:53:28 -0300

    Updateee

--- PJD4V/Assets/Scripts/EnemyController.cs
+++ PJD4V/Assets/Scripts/EnemyController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    
+    public Transform patrolPoint1;
+    public Transform patrolPoint2;
+    public float patrolSpeed = 2.0f;
+    public float patrolArrivalDistance = 0.1f;
+    
+    private Transform currentPatrolTarget;
+    private Vector3 originalScale;
+    
+    
 
     private Vector2 walkDirection;
 
@@ -15,13 +25,53 @@ public class EnemyController : MonoBehaviour
     void Start()
     {
         enemyAI = GetComponent<Animator>();
+        currentPatrolTarget = patrolPoint1;
+        originalScale = transform.localScale;
     }
 
     void Update()
     {
         MoveDirection();
         CountTime();
+        
+        if (Vector2.Distance(transform.position, currentPatrolTarget.position) <= patrolArrivalDistance)
+        {
+            // Troque o ponto de patrulha para o próximo
+            if (currentPatrolTarget == patrolPoint1)
+            {
+                currentPatrolTarget = patrolPoint2;
+                transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
+            }
+            else
+            {
+                currentPatrolTarget = patrolPoint1;
+                transform.localScale = originalScale;
+            }
+        }
+        
+        MoveTowardsTarget(currentPatrolTarget);
     }
+    
+    void MoveTowardsTarget(Transform target)
+    {
+        // Movimente o inimigo em direção ao ponto de patrulha com a velocidade de patrulha
+        float step = patrolSpeed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     public void SetWalkDirection(Vector2 direction) { //função pra mudar a direção
 
